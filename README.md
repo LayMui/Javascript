100dad44b5566e0baadcf21a7f3cac0e15798b11
LayMui <mui_ume@hotmail.com>
2022-07-20 13:31:37 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Lesson 1:
+```
+Development Tool
+Difference between compilation vs polyfilling
+Primitive and non-primitive types
+How variable are passed around: pass by reference and by value
+use strict
+```
---
+++ main.js
@@ -0,0 +1,37 @@
+
+'use strict'
+// function laymui(p1, p2) {
+//     console.log(this);
+// }
+
+// laymui.call()
+
+var person = {
+ 'name' : 'john'
+}
+// function foo(person) {
+// 	person = { 'name': 'mary'}
+// }
+// foo(person)
+// console.log(person.name)
+
+// var a = 1
+// function foo(a) {
+// 	a = 2
+// }
+
+// foo(a)
+// console.log(a)
+
+//  a = 1
+
+
+// console.log(a)
+
+// var theVal = 0
+// thVal  = 1
+// if (theVal > 0)
+//  console.log('theVal is greater than 0')
+var a = 2
+eval("var a = 1")
+console.log(a)
